11845292d74fedc7ac6135d2ad79b0c255189553
leorohr <rohrleo@googlemail.com>
2014-12-16 00:19:20 +0000

    cp

--- src/com/twacker/TwackerUI.java
+++ src/com/twacker/TwackerUI.java
@@ -2,21 +2,21 @@ package com.twacker;
 
 import javax.servlet.annotation.WebServlet;
 
-import com.twacker.views.SearchPage;
+import com.twacker.views.SearchView;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.navigator.Navigator;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
 
 @SuppressWarnings("serial")
 @Theme("twacker")
 public class TwackerUI extends UI {
 
+	Navigator navigator;
+	protected static final String MAINVIEW = "main";
+	
 	@WebServlet(value = "/*", asyncSupported = true)
 	@VaadinServletConfiguration(productionMode = false, ui = TwackerUI.class)
 	public static class Servlet extends VaadinServlet {
@@ -24,17 +24,12 @@ public class TwackerUI extends UI {
 
 	@Override
 	protected void init(VaadinRequest request) {
-		final VerticalLayout layout = new VerticalLayout();
-		layout.setMargin(true);
-		setContent(layout);
-
-		Button button = new Button("Click Me");
-		button.addClickListener(new Button.ClickListener() {
-			public void buttonClick(ClickEvent event) {
-				layout.addComponent(new Label("Thank you for clicking"));
-			}
-		});
-		layout.addComponent(button);
+		getPage().setTitle("Twacker");
+		
+		navigator = new Navigator(this, this);
+		navigator.addView("", new SearchView());
+//		navigator.addView(MAINVIEW, new MainView());
+	
 	}
 
 }
