147949d6a06c15cf65a4dda27f826d837982a6d3
leorohr <rohrleo@googlemail.com>
2015-07-05 10:10:22 +0100

    Added TopTweetsMsg value object

---
+++ src/main/java/org/twintiment/vo/TopTweetsMsg.java
@@ -0,0 +1,19 @@
+package org.twintiment.vo;
+
+public class TopTweetsMsg {
+	private TweetDataMsg[] topPosTweets;
+	private TweetDataMsg[] topNegTweets;
+	
+	public TopTweetsMsg(TweetDataMsg[] topPosTweets, TweetDataMsg[] topNegTweets) {
+		super();
+		this.topPosTweets = topPosTweets;
+		this.topNegTweets = topNegTweets;
+	}
+	public TweetDataMsg[] getTopPosTweets() {
+		return topPosTweets;
+	}
+	public TweetDataMsg[] getTopNegTweets() {
+		return topNegTweets;
+	}
+	
+}
